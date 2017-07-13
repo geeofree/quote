343d2bb3051f2bc48a9bf1ecb462394a5fd12a2e
Geoffrey <geo.galagaran@gmail.com>
2017-07-13 17:49:47 +0800

    Add tweeting button for quote

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import QuoteBtn from './quote.button'
+import TweetBtn from './tweet.button'
 
 const QuoteHeader = () => (
   <div className="quote-header">
     <h1 className="title">Quote.me</h1>
 
     <div className="header-buttons">
+      <TweetBtn />
       <QuoteBtn />
     </div>
   </div>
