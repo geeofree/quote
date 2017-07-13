0d85a447efe968ada45e33db2be9f425cdc97ca0
Geoffrey <geo.galagaran@gmail.com>
2017-07-13 14:08:19 +0800

    Use LoadingHOC component on Quote Component to render a something while it is fetching/loading resources

---
+++ src/components/loading.hoc.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchingQuote, dataReceived } from '../actions/quote.action'
+import request from '../api.request'
+
+
+const LoadingHOC = (WrappedComponent) => {
+  class Loader extends Component {
+    componentWillMount() {
+      const { toggleFetch, dataReceived } = this.props
+
+      toggleFetch(true)
+
+      request.get()
+      .then(response => {
+        dataReceived(response.status, response.data)
+        toggleFetch(false)
+      })
+      .catch(error => {
+        dataReceived(error.response.status, error.response.data)
+        toggleFetch(false)
+      })
+    }
+
+
+    render() {
+      const { isFetching } = this.props
+      return isFetching ? <p>Loading...</p> : <WrappedComponent {...this.props} />
+    }
+  }
+
+  const mapStateToProps = (state) => ({
+    isFetching: state.isFetching,
+    quote: state.quote
+  })
+
+  const mapDispatchToProps = (dispatch) => ({
+    toggleFetch: (bool) => dispatch(fetchingQuote(bool)),
+    dataReceived: (status, data) => dispatch(dataReceived(status, data))
+  })
+
+  return connect(mapStateToProps, mapDispatchToProps)(Loader)
+}
+
+export default LoadingHOC
--- src/components/quote.js
+++ src/components/quote.js
@@ -1,56 +1,22 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchingQuote, dataReceived } from '../actions/quote.action'
-import request from '../api.request'
+import LoadingHOC from './loading.hoc'
 
 
-class Quote extends Component {
-  componentWillMount() {
-    const { toggleFetch, dataReceived } = this.props
-
-    toggleFetch(true)
-
-    request.get()
-    .then(response => {
-      dataReceived(response.status, response.data)
-      toggleFetch(false)
-    })
-    .catch(error => {
-      dataReceived(error.response.status, error.response.data)
-      toggleFetch(false)
-    })
-  }
-
-  render() {
-    const { quote } = this.props
-
-    if(quote) {
-      if(quote.status === 200) {
-        return (
-          <div>
-            <p>{quote.data.quote}</p>
-            <p>{quote.data.author}</p>
-          </div>
-        )
-      }
-      else if(quote.status >= 400) {
-        return <p>Something went wrong with fetching :(</p>
-      }
+const Quote = ({ quote }) => {
+  if(quote) {
+    if(quote.status === 200) {
+      return (
+        <div>
+          <p>{quote.data.quote}</p>
+          <p>{quote.data.author}</p>
+        </div>
+      )
     }
-
-    return <p>placeholder</p>
+    else if(quote.status >= 400) {
+      return <p>Something went wrong with fetching :(</p>
+    }
   }
-
+  return null
 }
 
-const mapStateToProps = (state) => ({
-  isFetching: state.isFetching,
-  quote: state.quote
-})
-
-const mapDispatchToProps = (dispatch) => ({
-  toggleFetch: (bool) => dispatch(fetchingQuote(bool)),
-  dataReceived: (status, data) => dispatch(dataReceived(status, data))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Quote)
+export default LoadingHOC(Quote)
