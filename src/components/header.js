26dc660d66e153fd05b6641dbb27041c0ddcb94f
Geoffrey <geo.galagaran@gmail.com>
2017-07-13 17:15:28 +0800

    Add quote changer button

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import QuoteBtn from './quote.button'
 
 const QuoteHeader = () => (
   <div className="quote-header">
     <h1 className="title">Quote.me</h1>
+    <QuoteBtn />
   </div>
 )
 
---
+++ src/components/quote.button.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchingQuote, dataReceived } from '../actions/quote.action'
+import request from '../api.request'
+
+
+class NewQuoteBtn extends Component {
+  constructor(props) {
+    super(props)
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+
+  clickHandler(e) {
+    const { isFetching, toggleFetch, dataReceived } = this.props
+
+    toggleFetch(true)
+
+    if(!isFetching) {
+      request.get()
+      .then(response => {
+        dataReceived(response.status, response.data)
+        toggleFetch(false)
+      })
+      .catch(error => {
+        const { response } = error
+        dataReceived(response.status, response.data)
+        toggleFetch(false)
+      })
+    }
+  }
+
+
+  render() {
+    const { clickHandler } = this
+    return <button className="new-quote-button" onClick={clickHandler}>Change</button>
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isFetching: state.isFetching
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleFetch: (bool) => dispatch(fetchingQuote(bool)),
+  dataReceived: (status, data) => dispatch(dataReceived(status, data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewQuoteBtn)
