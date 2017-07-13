fc3b0474c31adedc01670e5d89eee37694914dff
Geoffrey <geo.galagaran@gmail.com>
2017-07-13 18:52:40 +0800

    Remove logging middleware

--- src/app.state.js
+++ src/app.state.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import QuoteReducer from './reducers/quote.reducer'
 
-const logger = (store) => (next) => (action) => {
-  console.log('Dispatching action:', action)
-  next(action)
-}
-
-const middleware =  applyMiddleware(logger)
-const appState = createStore(QuoteReducer, {}, middleware)
+// const logger = (store) => (next) => (action) => {
+//   console.log('Dispatching action:', action)
+//   next(action)
+// }
+//
+// const middleware =  applyMiddleware(logger)
+const appState = createStore(QuoteReducer)
 export default appState
